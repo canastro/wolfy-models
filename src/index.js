8e8d174658be50e1324694c1c31a5dab1dda3da2
Ricardo Canastro <ricardo.canastro@blip.pt>
2016-10-23 10:33:06 +0100

    Export models

--- src/index.js
+++ src/index.js
@@ -1,7 +1,27 @@
 const graffitiSchema = require('./schema/graffiti-schema');
 const graphQLSchema = require('./schema/graphql-schema');
 
+const Article = require('./schema/article');
+const Job = require('./schema/job');
+const Order = require('./schema/order');
+const Price = require('./schema/price');
+const Rating = require('./schema/rating');
+const SentimentReport = require('./schema/sentiment-report');
+const Stock = require('./schema/stock');
+const Subscriber = require('./schema/subscriber');
+const Tweet = require('./schema/tweet');
+
 module.exports = {
     graffitiSchema,
-    graphQLSchema
+    graphQLSchema,
+
+    Article,
+    Job,
+    Order,
+    Price,
+    Rating,
+    SentimentReport,
+    Stock,
+    Subscriber,
+    Tweet
 };
