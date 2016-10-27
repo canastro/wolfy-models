a15847490796ed673a5085968f7f3dd52b55d4b1
Ricardo Canastro <ricardo.canastro@blip.pt>
2016-10-27 17:01:32 +0100

    Move away from graffitti

--- src/index.js
+++ src/index.js
@@ -1,29 +1,9 @@
-const graffitiSchema = require('./schema/graffiti-schema');
-const graphQLSchema = require('./schema/graphql-schema');
+'use strict';
 
-const Article = require('./schema/article');
-const Candle = require('./technical-analysis/candle');
-const Job = require('./schema/job');
-const Order = require('./schema/order');
-const Price = require('./schema/price');
-const Rating = require('./schema/rating');
-const SentimentReport = require('./schema/sentiment-report');
-const Stock = require('./schema/stock');
-const Subscriber = require('./schema/subscriber');
-const Tweet = require('./schema/tweet');
+const mongo = require('./schema/mongo');
+const graphql = require('./schema/graphql');
 
 module.exports = {
-    graffitiSchema,
-    graphQLSchema,
-
-    Article,
-    Candle,
-    Job,
-    Order,
-    Price,
-    Rating,
-    SentimentReport,
-    Stock,
-    Subscriber,
-    Tweet
+    mongo,
+    graphql
 };
---
+++ src/schema/graphql/index.js
@@ -0,0 +1,7 @@
+const Order = require('./order');
+const Price = require('./price');
+
+module.exports = {
+    Order,
+    Price
+};
